d0158de343bba6c2e6e06dc7516fb66804868e64
Judson Paiva <judson.paiva@hotmail.com>
2021-08-30 22:05:30 +0100

    formulário criado

--- src/main/kotlin/Executar.kt
+++ src/main/kotlin/Executar.kt
@@ -1,5 +1,10 @@
+import swing.Formulario
 
+fun main() {
 
+    Formulario()
+
+}
 
 
 
--- src/main/kotlin/swing/Formulario.java
+++ src/main/kotlin/swing/Formulario.java
@@ -16,8 +16,35 @@ public class Formulario extends JFrame{
 
         super("Minha Janela");
         this.setSize(300, 150);
+        JPanel pn1 = new JPanel();
+        pn1.add(new JLabel("Valor 1"));
+        pn1.add(campoA);
 
+        JPanel pn2 = new JPanel();
+        pn2.add(new JLabel("Valor 2"));
+        pn2.add(campoB);
+
+        JPanel pn3 = new JPanel();
+        pn3.add(botao);
+
+        botao.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                executarAccao();
+            }
+        });
+
+        this.setLayout(new GridLayout(3, 1));
+        this.add(pn1);
+        this.add(pn2);
+        this.add(pn3);
+
+        this.setVisible(true);
     }
 
+
+    public void executarAccao(){
+        
+    }
 
 }
