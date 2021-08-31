ae948465dd83aec9ad10bbef42f0760d232ea4b4
Judson Paiva <judson.paiva@hotmail.com>
2021-08-31 01:19:37 +0100

    finalização do estudo teórico

--- src/main/kotlin/classesEinterfaces/Objecto.kt
+++ src/main/kotlin/classesEinterfaces/Objecto.kt
@@ -3,20 +3,35 @@ package classesEinterfaces
 class Aluno(var nome: String)
 
 interface A {
-    fun show()
+    fun show(): String
 }
 
 class Teste: A{
-    override fun show() {
-       println("mostrando método implementado ")
+    override fun show(): String {
+       return "mostrando método implementado "
     }
 }
 
+class Test: A{
+    override fun show() = "Teste em inglês"
+}
+
+
+fun passandoElementos(elemento: A){
+    println(elemento.show())
+}
 
 
 fun main() {
 
     var m = Teste()
+    var n: A = Teste()
+    var k: A = Test()
+
+    passandoElementos(m)
+    passandoElementos(k)
+    passandoElementos(n)
+
 
     /*
     var aluno = Aluno("Judson Paiva")
